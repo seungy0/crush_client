cffe9924a01828d6421618eecd1a7877772ec331
Ssoya1105 <hyun991105@naver.com>
2023-04-06 00:10:45 +0900

    feat: add swipe
    스와이프 추가와 메인페이지 대신 남들의 코디 변경

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,3 +1,4 @@
+import 'package:crush_client/pages/peoples_page.dart';
 import 'package:flutter/material.dart';
 import 'package:crush_client/pages/coodinator_page.dart';
 import 'package:crush_client/pages/cloth_page.dart';
@@ -9,43 +10,42 @@ class Mainpage extends StatefulWidget {
 }
 
 class _MainpageState extends State<Mainpage> {
-  int _selectedIndex = 0;
-  List<BottomNavigationBarItem> bottomItems=[
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.checkroom_outlined),
-      label: '코디네이터',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.checkroom_outlined),
-      label: '나의 옷장',
-    ),
-    const BottomNavigationBarItem(
-      icon: Icon(Icons.checkroom_outlined),
-      label: '마이 페이지',
-    ),
-  ];
   List<Widget> pages =
-  [ CoodinatorPage(),  ClosetPage(),  MyPage(),];
+  [ CoodinatorPage(),  ClosetPage(),  PeoplesPage(),];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.grey,
-        selectedItemColor: Colors.blueAccent,
-        unselectedItemColor: Colors.blue,
-        selectedFontSize: 14,
-        unselectedFontSize: 14,
-        currentIndex: _selectedIndex,
-        onTap: (int index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: bottomItems,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: TabBarView(children: [ CoodinatorPage(),  ClosetPage(),  PeoplesPage(),]),
+        bottomNavigationBar: Container(
+          height: 60,
+          color: Colors.grey,
+          child: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.checkroom_outlined),
+                text: '나의 코디',
+              ),
+              Tab(
+                icon: Icon(Icons.checkroom_outlined),
+                text: '나의 옷장',
+              ),
+              Tab(
+                icon: Icon(Icons.checkroom_outlined),
+                text: '남들의 코디',
+              ),
+            ],
+            padding: EdgeInsets.only(bottom: 1, top: 1),
+            labelColor: Colors.blueAccent,
+            unselectedLabelColor: Colors.lightBlueAccent,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.blueAccent,
+          ),
+        ),
       ),
-      body: pages[_selectedIndex],
     );
   }
 }
---
+++ lib/pages/peoples_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PeoplesPage extends StatefulWidget{
+  @override
+  State<PeoplesPage> createState() => _PeoplesPageState();
+}
+
+
+@override
+class _PeoplesPageState extends State<PeoplesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('다른 사람 코디'),
+      ),
+    );
+  }
+}
