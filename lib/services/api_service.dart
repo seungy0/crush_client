76a108049603e27ded71a7fcdd6917e38e8381c8
royalex1003 <59247797+royalex1003@users.noreply.github.com>
2023-04-10 15:51:08 +0900

    fix: https를 http로 변경

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,22 +4,15 @@ import 'package:crush_client/models/coordination_model.dart';
 import 'package:dio/dio.dart';
 
 class ApiService {
-  static const String baseUrl = "https://localhost";
+  static const String baseUrl = "http://10.0.2.2:8080";
   static const String recommend = "recommend";
   static const data = {
-    "cloths": [
-      {"name": "옷1", "color": "빨강", "type": "티셔츠", "thickness": "두꺼움"},
-      {"name": "자라에서 산거", "color": "파랑", "type": "바지", "thickness": "얇음"}
-    ],
-    "options": {
-      "weather": "sunny",
-      "occasion": "casual",
-      "season": "spring",
-      "style": "sporty"
-    }
+    "role" :"user",
+    "content":"{\"cloths\": [{\"name\": \"옷1\", \"color\": \"빨강\", \"type\": \"티셔츠\", \"thickness\": \"두꺼움\"},{\"name\": \"자라에서 산거\", \"color\": \"파랑\", \"type\": \"바지\", \"thickness\": \"얇음\"}],\"options\": {\"weather\": \"sunny\",\"occasion\": \"casual\",\"season\": \"spring\",\"style\": \"sporty\"}}"
   };
 
   static Future<List<CoordinationModel>> getAiCoordination() async {
+    print(data);
     List<CoordinationModel> coordinationInstances = [];
     final dio = Dio();
     final response = await dio.post(
@@ -28,7 +21,8 @@ class ApiService {
       options: Options(
         contentType: 'application/json',
       ),
-    );
+      );
+    print(response.data.toString());
     if (response.statusCode == 200) {
       final List<dynamic> aiCoordis = jsonDecode(response.data);
       for (var aiCoordi in aiCoordis) {
