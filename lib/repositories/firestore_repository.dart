9ca55c6fd441461b0b657e656f2f2c3b14ffe42b
seungy0 <dltmdrb98@gmail.com>
2023-04-30 21:22:15 +0900

    feat: firestore repository 작성, authentication repository에 getter 추가
    firestore repository작성
    작성한 메서드
    - Users - uid document 초기화
    - Users - uid
    - clothes subcollection에 옷 추가
    
    authentication repository 에 getter 추가
    -
    uid
    - user name
    - user email

--- lib/repositories/firestore_repository.dart
+++ lib/repositories/firestore_repository.dart
@@ -1,7 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../closet/model/cloth_model.dart';
+
 class FirestoreRepository {
   FirestoreRepository({required FirebaseFirestore firebaseFirestore})
       : _firebaseFirestore = firebaseFirestore;
   final FirebaseFirestore _firebaseFirestore;
+
+  // init Document
+  Future<void> initDocument({
+    required String document,
+    required String name,
+    required String email,
+  }) async {
+    await _firebaseFirestore.collection('Users').doc(document).set({
+      'name': name,
+      'email': email,
+      'age': 26, // TODO: 나이 입력받기
+      'sex': 'male', // TODO: 성별 입력받기
+    }, SetOptions(merge: true));
+  }
+
+  Future<void> addCloth({
+    required String uid,
+    required Cloth cloth,
+  }) async {
+    await _firebaseFirestore
+        .collection('Users')
+        .doc(uid)
+        .collection('clothes')
+        .add({
+      'name': cloth.name,
+      'color': cloth.color,
+      'type': cloth.type,
+      'thickness': cloth.thickness,
+    });
+  }
 }
