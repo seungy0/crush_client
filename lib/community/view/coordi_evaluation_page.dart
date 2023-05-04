c23942d1dc853cdcc3cde97e18d030d0da6145fb
royalex1003 <59247797+royalex1003@users.noreply.github.com>
2023-05-04 23:32:38 +0900

    feat: 타인 코디 UI 디자인 수정
    사진 크기 크게

--- lib/community/view/coordi_evaluation_page.dart
+++ lib/community/view/coordi_evaluation_page.dart
@@ -11,60 +11,60 @@ class _CoordiEvalPageState extends State<CoordiEvalPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Stack(
-        children: [
-          Container(
-            height: 550,
-            width: 300,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                  image: AssetImage('assets/saeroy.png'),
-                  fit: BoxFit.cover,
-                )),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 5,),
-              child: Column(
-                children: [
-                  SizedBox(height: 465,),
-                  Container(
-                    decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.25),
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: RatingBar(
-                        initialRating: 0,
-                        direction: Axis.horizontal,
-                        allowHalfRating: false,
-                        itemCount: 5,
-                        ratingWidget: RatingWidget(
-                          full: Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          ),
-                          half: Icon(
-                            Icons.star_half,
-                            color: Colors.amber,
-                          ),
-                          empty: Icon(
-                            Icons.star,
-                            color: Colors.grey,
-                          ),
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.95,
+        width: MediaQuery.of(context).size.width * 0.95,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage('assets/saeroy.png'),
+              fit: BoxFit.cover,
+            )),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8,),
+          child: Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.65,),
+              FittedBox(
+                fit: BoxFit.fitWidth,
+                child: Container(
+                  decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.25),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: RatingBar(
+                      initialRating: 0,
+                      direction: Axis.horizontal,
+                      allowHalfRating: false,
+                      itemCount: 5,
+                      ratingWidget: RatingWidget(
+                        full: Icon(
+                          Icons.star,
+                          color: Colors.amber,
                         ),
-                        itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                        itemSize: 45,
-                        onRatingUpdate: (rating) {
-                          print(rating);
-                        },
+                        half: Icon(
+                          Icons.star_half,
+                          color: Colors.amber,
+                        ),
+                        empty: Icon(
+                          Icons.star,
+                          color: Colors.grey,
+                        ),
                       ),
+                      itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                      itemSize: 80,
+                      glow: false,
+                      onRatingUpdate: (rating) {
+                        print(rating);
+                      },
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          )
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
