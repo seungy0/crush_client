a6f6c1a0d9751b058474e1f990373e9d002a5a8e
royalex1003 <59247797+royalex1003@users.noreply.github.com>
2023-05-08 02:01:48 +0900

    feat: 타인 코디 UI 디자인 수정
    별점 부분 그라데이션으로 변경

--- lib/community/view/coordi_evaluation_page.dart
+++ lib/community/view/coordi_evaluation_page.dart
@@ -14,56 +14,83 @@ class _CoordiEvalPageState extends State<CoordiEvalPage> {
       child: Container(
         height: MediaQuery.of(context).size.height * 0.95,
         width: MediaQuery.of(context).size.width * 0.95,
+        clipBehavior: Clip.hardEdge,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-              image: AssetImage('assets/saeroy.png'),
-              fit: BoxFit.cover,
-            )),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8,),
-          child: Column(
-            children: [
-              SizedBox(height: MediaQuery.of(context).size.height * 0.65,),
-              FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Container(
-                  decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.25),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: RatingBar(
-                      initialRating: 0,
-                      direction: Axis.horizontal,
-                      allowHalfRating: false,
-                      itemCount: 5,
-                      ratingWidget: RatingWidget(
-                        full: Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                        ),
-                        half: Icon(
-                          Icons.star_half,
-                          color: Colors.amber,
-                        ),
-                        empty: Icon(
-                          Icons.star,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
-                      itemSize: 80,
-                      glow: false,
-                      onRatingUpdate: (rating) {
-                        print(rating);
-                      },
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            image: AssetImage('assets/saeroy.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.075,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: <Color>[
+                        Colors.black.withOpacity(0),
+                        Colors.black.withOpacity(1),
+                      ],
                     ),
                   ),
                 ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.075,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.0,
+                horizontal: 15,
               ),
-            ],
-          ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      RatingBar(
+                        initialRating: 0,
+                        direction: Axis.horizontal,
+                        allowHalfRating: false,
+                        itemCount: 5,
+                        ratingWidget: RatingWidget(
+                          full: Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          half: Icon(
+                            Icons.star_half,
+                            color: Colors.amber,
+                          ),
+                          empty: Icon(
+                            Icons.star,
+                            color: Colors.grey.shade700,
+                          ),
+                        ),
+                        itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                        itemSize: MediaQuery.of(context).size.width * 0.15,
+                        glow: false,
+                        onRatingUpdate: (rating) {
+                          print(rating);
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
