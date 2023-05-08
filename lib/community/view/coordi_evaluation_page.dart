425dce3bc5cbbf1697b7695667f0a446201ff708
royalex1003 <59247797+royalex1003@users.noreply.github.com>
2023-05-08 16:14:37 +0900

    feat: 타인 코디 UI 디자인 수정

--- lib/community/view/coordi_evaluation_page.dart
+++ lib/community/view/coordi_evaluation_page.dart
@@ -10,89 +10,99 @@ class CoordiEvalPage extends StatefulWidget {
 class _CoordiEvalPageState extends State<CoordiEvalPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: MediaQuery.of(context).size.height * 0.95,
-        width: MediaQuery.of(context).size.width * 0.95,
-        clipBehavior: Clip.hardEdge,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          image: DecorationImage(
-            image: AssetImage('assets/saeroy.png'),
-            fit: BoxFit.cover,
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Container(
+          height: screenHeight * 0.8,
+          width: screenWidth * 0.95,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
           ),
-        ),
-        child: Stack(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.075,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: <Color>[
-                        Colors.black.withOpacity(0),
-                        Colors.black.withOpacity(1),
-                      ],
-                    ),
+          child: Stack(
+            children: [
+              Container(
+                height: screenHeight * 0.75,
+                width: screenWidth * 0.95,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/musinsa.jpg'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.075,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                  ),
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 15,
               ),
-              child: Column(
+              Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RatingBar(
-                        initialRating: 0,
-                        direction: Axis.horizontal,
-                        allowHalfRating: false,
-                        itemCount: 5,
-                        ratingWidget: RatingWidget(
-                          full: Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          ),
-                          half: Icon(
-                            Icons.star_half,
-                            color: Colors.amber,
-                          ),
-                          empty: Icon(
-                            Icons.star,
-                            color: Colors.grey.shade700,
-                          ),
-                        ),
-                        itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
-                        itemSize: MediaQuery.of(context).size.width * 0.15,
-                        glow: false,
-                        onRatingUpdate: (rating) {
-                          print(rating);
-                        },
+                  Container(
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                          Colors.black.withOpacity(0),
+                          Colors.black.withOpacity(1),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
+                  Container(
+                    height: screenHeight * 0.05,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                  )
                 ],
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 15,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RatingBar(
+                          initialRating: 0,
+                          direction: Axis.horizontal,
+                          allowHalfRating: false,
+                          itemCount: 5,
+                          ratingWidget: RatingWidget(
+                            full: Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            half: Icon(
+                              Icons.star_half,
+                              color: Colors.amber,
+                            ),
+                            empty: Icon(
+                              Icons.star,
+                              color: Colors.grey.shade700,
+                            ),
+                          ),
+                          itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                          itemSize: screenHeight * 0.065,
+                          glow: false,
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
