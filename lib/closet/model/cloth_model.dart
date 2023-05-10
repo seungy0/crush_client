7de72ef8bc67fe06bec3319218c68f471b30cb5e
seungy0 <dltmdrb98@gmail.com>
2023-05-10 18:27:18 +0900

    feat: Cloth 객체에 Cloth Id 값 추가
    Cloth id 를 옷 변경, 삭제에 사용하기 위함

--- lib/closet/model/cloth_model.dart
+++ lib/closet/model/cloth_model.dart
@@ -1,10 +1,12 @@
 class Cloth {
+  String? clothId;
   final String name;
   final String color;
   final String type;
   final String thickness;
 
   Cloth({
+    required this.clothId,
     required this.name,
     required this.color,
     required this.type,
@@ -13,16 +15,19 @@ class Cloth {
 
   factory Cloth.fromJson(Map<String, dynamic> json) {
     return Cloth(
-      name: json['name']??'No Name',
+      clothId: json['clothId'] ?? 'No ClothId',
+      name: json['name'] ?? 'No Name',
       color: json['color'],
       type: json['type'],
       thickness: json['thickness'],
     );
   }
+
   Map<String, dynamic> toJson() => {
-    'name': name,
-    'color': color,
-    'type': type,
-    'thickness': thickness,
-  };
+        'clothId': clothId,
+        'name': name,
+        'color': color,
+        'type': type,
+        'thickness': thickness,
+      };
 }
