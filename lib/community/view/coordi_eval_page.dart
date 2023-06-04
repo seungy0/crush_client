9b29e95ca8806dcd848b59fe9c9eb8f05e69a3eb
andysooho <andysooho@gmail.com>
2023-06-04 21:44:34 +0900

    refactor: 평가항목간에 앱 로고 나오게 수정
    1.평가 항목간에 앱 로고 나오게 수정.
    2.불필요 asset 제거 및 정리
    
    #48

--- lib/community/view/coordi_eval_page.dart
+++ lib/community/view/coordi_eval_page.dart
@@ -45,62 +45,96 @@ class _CoordiEvalPageState extends State<CoordiEvalPage> {
       title: '코디 평가',
       child: _finished
           ? Container(
-        height: MediaQuery.of(context).size.height * 0.5,
-        alignment: Alignment.center,
-        child: const Text(
-          '\n\n평가가 완료되었습니다.\n\n😁🥰',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-          textAlign: TextAlign.center,
-        ),
-      )
-          : FutureBuilder<List<MyOutfit>>(
-        future: _outfitList,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Container(
-                height: MediaQuery.of(context).size.height * 0.5,
-                alignment: Alignment.center,
-                child: const CircularProgressIndicator());
-          }
-          List<MyOutfit>? outfits = snapshot.data;
-          if (outfits == null || outfits.isEmpty) {
-            return Container(
               height: MediaQuery.of(context).size.height * 0.5,
               alignment: Alignment.center,
-              child: const Text('평가할 코디가 없습니다.'),
-            );
-          } else {
-            //Prefetch images
-            Future.wait(prefetchImages(outfits));
-
-            return Stack(
-              children: outfits.asMap().entries.map((e) {
-                int index = e.key;
-                MyOutfit outfit = e.value;
-                return Visibility(
-                  visible: index == _curIndex,
-                  child: CoordiEvalCard(
-                    photoUri: outfit.photoUrl,
-                    title: outfit.title,
-                    onRated: (double rating) async {
-                      await _coordiRepository.rateOutfit(
-                          coordid: outfit.coordiId,
-                          raterUserId: userId,
-                          stars: rating);
-                      setState(() {
-                        _curIndex += 1;
-                        if (_curIndex >= outfits.length) {
-                          _finished = true;
-                        }
-                      });
-                    },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    '평가가 완료되었습니다.\n',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    textAlign: TextAlign.center,
                   ),
-                );
-              }).toList(),
-            );
-          }
-        },
-      ),
+                  Text(
+                    '😁🥰',
+                    style: TextStyle(fontSize: 45),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            )
+          : FutureBuilder<List<MyOutfit>>(
+              future: _outfitList,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container(
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      alignment: Alignment.center,
+                      child: const CircularProgressIndicator());
+                }
+
+                List<MyOutfit>? outfits = snapshot.data;
+                if (outfits == null || outfits.isEmpty) {
+                  return Container(
+                    height: MediaQuery.of(context).size.height * 0.5,
+                    alignment: Alignment.center,
+                    child: const Text('평가할 코디가 없습니다.'),
+                  );
+                } else {
+                  //Prefetch images
+                  Future.wait(prefetchImages(outfits));
+
+                  return Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Image.asset(
+                                'assets/logo_circle.png',
+                                fit: BoxFit.fitWidth,
+                                width: MediaQuery.of(context).size.width * 0.7,
+                                height: MediaQuery.of(context).size.height * 0.7,
+                                alignment: Alignment.center,
+                            ),
+                            Text(
+                              '${_curIndex + 1} 번째 평가중 / 총 ${outfits.length}개',
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ...outfits.asMap().entries.map((e) {
+                        int index = e.key;
+                        MyOutfit outfit = e.value;
+                        return Visibility(
+                          visible: index == _curIndex,
+                          child: CoordiEvalCard(
+                            photoUri: outfit.photoUrl,
+                            title: outfit.title,
+                            onRated: (double rating) async {
+                              await _coordiRepository.rateOutfit(
+                                  coordid: outfit.coordiId,
+                                  raterUserId: userId,
+                                  stars: rating);
+                              setState(() {
+                                _curIndex += 1;
+                                if (_curIndex >= outfits.length) {
+                                  _finished = true;
+                                }
+                              });
+                            },
+                          ),
+                        );
+                      }).toList(),
+                    ],
+                  );
+                }
+              },
+            ),
     );
   }
 }
